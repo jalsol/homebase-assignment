3e9665be5ceb3ce26aa1ad5ddef744d1348466f3
jalsol <jalsol@pm.me>
2023-12-30 10:42:45 +0700

    Add: Task 3 Cookie Storing and Product URL Scraping

--- task3/main.py
+++ task3/main.py
@@ -1,2 +1,17 @@
+from scraper import Scraper
+
+import logging
+
+
+URL = "https://batdongsan.com.vn/cho-thue-nha-tro-phong-tro-quan-8/gia-tu-1-trieu-den-3-trieu?disIds=62,57"
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+
+    scraper = Scraper(URL)
+    scraper.scrape()
+
+
 if __name__ == '__main__':
-    pass
+    main()
--- task3/scraper.py
+++ task3/scraper.py
@@ -1,2 +1,47 @@
+from bs4 import BeautifulSoup
+from product_parser import ProductParser
+
+import logging
+import undetected_chromedriver as uc
+import time
+
+
+CHROMIUM_EXECUTABLE_PATH = '/usr/bin/brave-browser'
+USER_DATA_DIR = '/home/jalsol/.config/BraveSoftware/Brave-Browser'
+CLOUDFLARE_TIMEOUT = 5
+
+
 class Scraper:
-    pass
+    def __init__(self, url: str):
+        logging.info('Scraper initialized')
+        logging.info(f'Chromium path: {CHROMIUM_EXECUTABLE_PATH}')
+        logging.info(f'User data dir: {USER_DATA_DIR}')
+        logging.debug(f'URL: {url}')
+        self.url = url
+        self.cookies = None
+        self.page_source = None
+        self.driver = uc.Chrome(use_subprocess=True,
+                                browser_executable_path=CHROMIUM_EXECUTABLE_PATH,
+                                user_data_dir=USER_DATA_DIR)
+
+    def _load_url(self):
+        logging.info('Loading URL...')
+
+        self.driver.get(self.url)
+        time.sleep(CLOUDFLARE_TIMEOUT)
+
+        self.page_source = self.driver.page_source
+        self.cookies = self.driver.get_cookies()
+        logging.debug(f'Cookies: {self.cookies}')
+
+        self.driver.close()
+
+    def scrape(self):
+        self._load_url()
+
+        soup = BeautifulSoup(self.page_source, 'html.parser')
+        products = soup.find_all(class_='js__product-link-for-product-id')
+
+        for product in products:
+            product_url = product['href']
+            logging.debug(f'Product URL: {product_url}')
