3246c0a37bb1aacbd0c87591990fb212b1aefb97
jalsol <jalsol@pm.me>
2023-12-30 12:32:38 +0700

    Add: Task 3 Add Product Parser to Scraper

--- task3/scraper.py
+++ task3/scraper.py
@@ -1,18 +1,19 @@
 from bs4 import BeautifulSoup
 from product_parser import ProductParser
 
+import json
 import logging
-import undetected_chromedriver as uc
 import time
+import undetected_chromedriver as uc
 
 
-CHROMIUM_EXECUTABLE_PATH = '/usr/bin/brave-browser'
-USER_DATA_DIR = '/home/jalsol/.config/BraveSoftware/Brave-Browser'
+CHROMIUM_EXECUTABLE_PATH = '/usr/bin/brave-browser-beta'
+USER_DATA_DIR = '/home/jalsol/.config/BraveSoftware/Brave-Browser-Beta'
 CLOUDFLARE_TIMEOUT = 5
 
 
 class Scraper:
-    def __init__(self, url: str):
+    def __init__(self, url: str, output_file: str = 'data.json'):
         logging.info('Scraper initialized')
         logging.info(f'Chromium path: {CHROMIUM_EXECUTABLE_PATH}')
         logging.info(f'User data dir: {USER_DATA_DIR}')
@@ -23,18 +24,23 @@ class Scraper:
         self.driver = uc.Chrome(use_subprocess=True,
                                 browser_executable_path=CHROMIUM_EXECUTABLE_PATH,
                                 user_data_dir=USER_DATA_DIR)
+        self.data = []
+
+    def _set_cookies(self, cookies):
+        self.cookies = cookies
+        logging.debug(f'Cookies: {self.cookies}')
+        for cookie in self.cookies:
+            self.driver.add_cookie(cookie)
 
     def _load_url(self):
         logging.info('Loading URL...')
-
         self.driver.get(self.url)
-        time.sleep(CLOUDFLARE_TIMEOUT)
+
+        if self.cookies is None:
+            time.sleep(CLOUDFLARE_TIMEOUT)
+            self._set_cookies(self.driver.get_cookies())
 
         self.page_source = self.driver.page_source
-        self.cookies = self.driver.get_cookies()
-        logging.debug(f'Cookies: {self.cookies}')
-
-        self.driver.close()
 
     def scrape(self):
         self._load_url()
@@ -45,3 +51,14 @@ class Scraper:
         for product in products:
             product_url = product['href']
             logging.debug(f'Product URL: {product_url}')
+
+            self.driver.get(f'https://batdongsan.com.vn{product_url}')            
+            product_parser = ProductParser(self.driver.page_source)
+            product_parser.parse()
+            self.data.append(product_parser.data)
+        
+        with open('data.json', 'w') as out_file:
+            json.dump(self.data, out_file, indent=2, ensure_ascii=False)
+
+        self.driver.close()
+
